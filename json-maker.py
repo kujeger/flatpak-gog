1976ae6ae684bf0f3d790b6a31bed2b9d1118836
Kai A. Hiller <V02460@gmail.com>
2020-01-31 17:28:24 +0100

    Create Template class

--- json-maker.py
+++ json-maker.py
@@ -7,45 +7,72 @@ import os
 import re
 import zipfile
 from collections import OrderedDict
-from typing import Dict, Mapping, Optional, Sequence, TextIO
+from dataclasses import dataclass, field
+from typing import Any, Callable, List, Mapping, Optional, Sequence, TextIO
+
+from typing_extensions import Literal
 
 logging.basicConfig(format="%(levelname)s: %(message)s")
 
 DEFAULT_TEMPLATE = "com.gog.Template.json"
 I386_COMPAT_TEMPLATE = "com.gog.i386-compat.Template.json"
 
+Json = OrderedDict  # [str, Any]
+Transform = Callable[[Json], Json]
+Arch = Literal["i386", "i386+x86_64", "x86_64"]
+InstallerType = Literal["mojo"]
 
-def getGameInfo(installer, argname, argarch, archdata):
-    gameinfo = {}
-    with zipfile.ZipFile(installer, 'r') as myzip:
-        with myzip.open('data/noarch/gameinfo') as myfile:
-            tmplist = myfile.read().decode('utf-8').split('\n')
-            gameinfo['orig-name'] = tmplist[0]
-            gameinfo['gogversion'] = tmplist[1]
-            gameinfo['version'] = tmplist[2]
-        gogversiondate = myzip.getinfo('data/noarch/gameinfo').date_time
-        gameinfo['gogversiondate'] = "{}-{}-{}".format(gogversiondate[0], gogversiondate[1], gogversiondate[2])
 
-    gameinfo['name'] = sanitizedName(gameinfo['orig-name'])
-    gameinfo['app-id'] = appIDFromName(gameinfo['orig-name'])
+@dataclass
+class GameInfo:
+    origname: str
+    gogversion: str
+    version: str
+    # arch parameter requires Optional, but arch attribute doesn't
+    _arch: Optional[Arch] = field(repr=False, compare=False, hash=False)
+    gogversiondate: str
+    typ: InstallerType
+    name: str = field(init=False)
+    appid: str = field(init=False)
+    arch: Arch = field(init=False)
 
-    if argname != 'auto':
-        gameinfo['name'] = argname
+    def __post_init__(self) -> None:
+        argname = args.name if args.name != "auto" else None
+        argarch = args.arch if args.arch != "auto" else None
+        self.origname = argname or self.origname
+        self.name = sanitizedName(self.origname)
+        self.appid = appIDFromName(self.origname)
+        self.arch = argarch or self._arch or self._lookupArch(self.name)
 
-    if argarch == 'auto':
-        gameinfo['arch'] = archdata.get(gameinfo['name'])
-        if not gameinfo['arch']:
+    @staticmethod
+    def fromMojoSetup(installer: str) -> "GameInfo":
+        """Get game info from the Mojo Setup format used by GOG Linux installers."""
+        with zipfile.ZipFile(installer, 'r') as myzip:
+            with myzip.open('data/noarch/gameinfo') as myfile:
+                tmplist = myfile.read().decode('utf-8').split('\n')
+                name = tmplist[0]
+                gogversion = tmplist[1]
+                version = tmplist[2]
+                if version == 'na':
+                    version = '1.0'
+            filedatetime = myzip.getinfo('data/noarch/gameinfo').date_time
+        gogversiondate = "{0}-{1}-{2}".format(*filedatetime)
+
+        return GameInfo(name, gogversion, version, None, gogversiondate, "mojo")
+
+    @staticmethod
+    def _lookupArch(gamename: str) -> Arch:
+        with open('archlist.json', 'r') as archfile:
+            archdata = json.load(archfile)
+
+        arch = archdata.get(gamename)
+        if not arch:
             logging.warning(
                 "Arch not specified, and not found in archlist.json - "
                 "defaulting to x86_64!"
             )
-            gameinfo['arch'] = 'x86_64'
-    else:
-        gameinfo['arch'] = argarch
-    if gameinfo['arch'] == 'i386+x86_64':
-            gameinfo['arch'] = 'x86_64'
-
-    return gameinfo
+            arch = "x86_64"
+        return arch
 
 
 def sanitizedName(name: str) -> str:
@@ -63,6 +90,71 @@ def appIDFromName(name: str) -> str:
         return 'com.gog._' + namecc
     else:
         return 'com.gog.' + namecc
+
+
+class Template:
+    """Flatpak builder template based on gameinfo data.
+
+    Rendering the template loads JSON data from a template file, applies all
+    given transforms and replaces placeholders with data from gameinfo.
+    """
+
+    def __init__(self, gameinfo: GameInfo, transforms: List[Transform] = []) -> None:
+        self.gameinfo = gameinfo
+        # Last transform is the string replacement
+        self.transforms = transforms + [self._render]
+        self.replace_dict: Mapping[str, str] = {
+            "APPID": self.gameinfo.appid,
+            "GAMENAME": self.gameinfo.name,
+            "REPLACELONGNAME": self.gameinfo.origname,
+            "REPLACESHORTNAME": self.gameinfo.name,
+            "REPLACEVERSIONSTRING": self.gameinfo.gogversion,
+            "REPLACEVERSIONDATE": self.gameinfo.gogversiondate,
+        }
+
+    def transform(self, transform: Transform) -> "Template":
+        """Returns a new template with the transform applied on top."""
+        return Template(self.gameinfo, self.transforms[:-1] + [transform])
+
+    def render(self) -> Json:
+        """Returns a rendered JSON object.
+
+        The returned JSON object has all transformations applied to the template
+        and all placeholder strings replaced by their real values.
+        """
+        jsondata = self._loadTemplateFromFile(self.gameinfo.arch)
+        transformed = self._applyTransforms(jsondata)
+        return transformed
+
+    def _applyTransforms(self, jsondata: Json) -> Json:
+        for t in self.transforms:
+            jsondata = t(jsondata)
+        return jsondata
+
+    def _render(self, x: Any) -> Any:
+        """Returns a JSON object with every replacement applied to every string."""
+
+        def replace_all(s: str) -> str:
+            for k, v in self.replace_dict.items():
+                s = s.replace(k, v)
+            return s
+
+        if isinstance(x, dict):
+            return OrderedDict((k, self._render(v)) for k, v in x.items())
+        if isinstance(x, list):
+            return [self._render(k) for k in x]
+        if isinstance(x, str):
+            return replace_all(x)
+        return x
+
+    @staticmethod
+    def _loadTemplateFromFile(arch: Arch) -> Json:
+        if args.template:
+            return json.load(args.template, object_pairs_hook=OrderedDict)
+
+        filename = I386_COMPAT_TEMPLATE if (arch == "i386") else DEFAULT_TEMPLATE
+        with open(filename, "r") as file:
+            return json.load(file, object_pairs_hook=OrderedDict)
 
 
 def parseArgs() -> argparse.Namespace:
@@ -119,7 +211,6 @@ args = parseArgs()
 
 def getGameModule(
     gamemodule_template: TextIO,
-    gameinfo: Mapping[str, str],
     installer: str,
     startoverride: str,
     configureoverride: str,
@@ -129,14 +220,6 @@ def getGameModule(
     moduledata = json.load(gamemodule_template, object_pairs_hook=OrderedDict)
 
     moduledata['sources'][0]['path'] = installer
-
-    buildcommands = moduledata['build-commands']
-    for idx, item in enumerate(buildcommands):
-        newstring = item.replace("REPLACELONGNAME", gameinfo['orig-name'])
-        newstring = newstring.replace("REPLACESHORTNAME", gameinfo['name'])
-        newstring = newstring.replace("REPLACEVERSIONSTRING", gameinfo['gogversion'])
-        newstring = newstring.replace("REPLACEVERSIONDATE", gameinfo['gogversiondate'])
-        buildcommands[idx] = newstring
 
     if os.path.isfile(startoverride):
         moduledata['sources'].append(
@@ -167,68 +250,46 @@ def getGameModule(
     return moduledata
 
 
-def readTemplate(arch: str, template: Optional[TextIO]) -> Dict:
-    """Read the content of the JSON template.
-
-    Notes:
-        - If arch is i386 the compatibility template is used.
-        - The template can always be overridden by `template`.
-    """
-    if template:
-        return json.load(template, object_pairs_hook=OrderedDict)
-    else:
-        filename = I386_COMPAT_TEMPLATE if (arch == "i386") else DEFAULT_TEMPLATE
-        with open(filename, 'r') as file:
-            return json.load(file, object_pairs_hook=OrderedDict)
-
-
 def main() -> None:
-    with open('archlist.json', 'r') as archfile:
-        archdata = json.load(archfile)
-
-    gameinfo = getGameInfo(
-            args.installer,
-            args.name,
-            args.arch,
-            archdata)
-
-    jsondata = readTemplate(gameinfo['arch'], args.template)
-
-    jsondata['app-id'] = gameinfo['app-id']
+    gameinfo = GameInfo.fromMojoSetup(args.installer)
 
     startoverride = args.startoverride
     configureoverride = args.configureoverride
     modulesoverride = 'auto'
     if startoverride == 'auto':
-        startoverride = "overrides/starter-{}".format(gameinfo['name'])
+        startoverride = "overrides/starter-{}".format(gameinfo.name)
     if configureoverride == 'auto':
-        configureoverride = "overrides/configure-{}".format(gameinfo['name'])
+        configureoverride = "overrides/configure-{}".format(gameinfo.name)
     if modulesoverride == 'auto':
-        modulesoverride = "overrides/modules-{}.json".format(gameinfo['name'])
+        modulesoverride = "overrides/modules-{}.json".format(gameinfo.name)
 
-    gamemodule = getGameModule(
-        args.gamemodule,
-        gameinfo,
-        args.installer,
-        startoverride,
-        configureoverride,
-        args.extra,
-    )
-    jsondata['modules'].insert(0, gamemodule)
+    def addGamemodule(jsondata: Json) -> Json:
+        gamemodule = getGameModule(
+            args.gamemodule,
+            args.installer,
+            startoverride,
+            configureoverride,
+            args.extra,
+        )
+        jsondata["modules"].insert(0, gamemodule)
+        return jsondata
 
-    if os.path.isfile(modulesoverride):
-        moduledata = "{}"
-        with open(modulesoverride, 'r') as f:
-            moduledata = json.load(f, object_pairs_hook=OrderedDict)
+    def moduleOverride(jsondata: Json) -> Json:
+        if os.path.isfile(modulesoverride):
+            with open(modulesoverride, "r") as f:
+                moduledata = json.load(f, object_pairs_hook=OrderedDict)
 
-        for module in moduledata:
-            jsondata['modules'].append(module)
+            for module in moduledata:
+                jsondata["modules"].append(module)
+        return jsondata
 
-    outname = ("gen_{}.json".format(gameinfo['app-id'])
+    template = Template(gameinfo).transform(addGamemodule).transform(moduleOverride)
+
+    outname = ("gen_{}.json".format(gameinfo.appid)
                if args.output == 'auto' else args.output)
 
     with open(outname, 'w') as outfile:
-        json.dump(jsondata, outfile, indent=4)
+        json.dump(template.render(), outfile, indent=4)
 
     if args.verbose > 0:
         print("JSON written to {1}\n"
